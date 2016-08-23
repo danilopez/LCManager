d474ae744159eb7f9fe18f0885d59c2a4eb23ffc
Daniel López <tauron32@gmail.com>
2016-08-23 20:00:16 +0200

    Move common files to common folder and add base file for db connection

--- common/navigation.php
+++ common/navigation.php
@@ -15,13 +15,7 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
+                    <a href="#">Add member</a>
                 </li>
             </ul>
         </div>
