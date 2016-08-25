9d9f354ad938da5d9fc3159761d2ca8846930404
Daniel Lopez <dlp@expandit.com>
2016-08-25 19:05:51 +0200

    Add mock pages

---
+++ structure/centres.php
@@ -0,0 +1,87 @@
+<?php 
+  $active = "centres";
+  $title = "Centres";
+
+	include_once '../common/header.php';
+	include_once '../common/navigation.php';
+  include_once '../common/sidebar.php'
+?>
+
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+  <h1 class="page-header"><?php echo $title ?></h1>
+
+    <div class="table-responsive">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>City</th>
+          <th>Province</th>
+          <th>Address</th>
+          <th>ZipCode</th>
+          <th>Phone</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Name</td>
+          <td>City</td>
+          <td>Province</td>
+          <td>Address</td>
+          <td>ZipCode</td>
+          <td>Phone</td>
+          <td>Email</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+          <td>City</td>
+          <td>Province</td>
+          <td>Address</td>
+          <td>ZipCode</td>
+          <td>Phone</td>
+          <td>Email</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+          <td>City</td>
+          <td>Province</td>
+          <td>Address</td>
+          <td>ZipCode</td>
+          <td>Phone</td>
+          <td>Email</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+          <td>City</td>
+          <td>Province</td>
+          <td>Address</td>
+          <td>ZipCode</td>
+          <td>Phone</td>
+          <td>Email</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+          <td>City</td>
+          <td>Province</td>
+          <td>Address</td>
+          <td>ZipCode</td>
+          <td>Phone</td>
+          <td>Email</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+          <td>City</td>
+          <td>Province</td>
+          <td>Address</td>
+          <td>ZipCode</td>
+          <td>Phone</td>
+          <td>Email</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+<?php
+  include_once '../common/footer.php'
+?>
